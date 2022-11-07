2d0a57bc7188d7e5949ad4f4fa4804c6fdf17167
mohSlm-7d <68254871+mohSlm-7d@users.noreply.github.com>
2022-11-07 19:16:05 +0300

    Update Solution.java

--- GfG/MergeSort in java/src/Solution.java
+++ GfG/MergeSort in java/src/Solution.java
@@ -12,83 +12,48 @@
 
 class Solution
 {
+    void Merge(int data[], int first, int mid,  int last){
+        int i= first, j= mid+1, k = 0;
+        int[] SubArray = new int[last - first +1];    
     
-    void merge(int arr[], int l, int m, int r)
-    {
-        
-         // Your code here
-         int size1= m - l +1;
-         int size2= r-m;
-         int [] first = new int [size1];
-         int[] last=new int[size2];
-         
-         int c=0;
-         while (c < size1){
-             first[c] = arr[l+c];
-             c++;
+        while(i<= mid && j<= last){
+            if(data[i] < data[j]){
+                SubArray[k++] = data[i++];
+            }
+            else if(data[j] < data[i]){
+                SubArray[k++] = data[j++];
+            }
+            else{
+                SubArray[k++]=data[i++];
+                SubArray[k++]=data[j++];
+            }
          }
-         
-         c=0;
-         while (c < size2){
-             last[c] = arr[m+1+c];
-             c++;
-         }
-         
-         int i=0, j=0, k=l;
-         while(i < size1 && j< size2){
-             if(last[j] < first[i]){
-                 arr[k]= last[j];
-                 
-                 j++;
-                 k++;
-             }
-             
-             else if(first[i] < last[j]){
-                arr[k]= first[i];
-                i++;
-                k++;
-             }
-             else{
-                 arr[k]= first[i];
-                 i++;
-                 k++;
-                 
-                 
-                 arr[k]= last[j];
-                 j++;
-                 k++;
-             }
-         }
-         
-         if(i < size1){
-             while(i < size1){
-                 arr[k]= first[i];
-                 i++;
-                 k++;
-             }
-         }
-         
-         else{
-             while(j< size2){
-                 arr[k]= last[j];
-                 j++;
-                 k++;
-             }
-         }
-    }
-    void mergeSort(int arr[], int l, int r)
-    {
-        //code here
-        
-        if( r > l){
-            int mid = (l+r) / 2;
-            mergeSort(arr, l, mid);
-            mergeSort(arr, mid+1, r);
-            merge(arr, l, mid, r);
+
+        while(i<= mid){
+            SubArray[k++]=data[i++];
         }
-        
-    }
+        while(j<= last){
+            SubArray[k++]=data[j++];
+        }
+
+        k=0;
+        i = first;
+        while(k < last - first +1){
+            data[i] = SubArray[k];
+            k++;
+            i++;
+        }
     
+    }
+    void mergeSort(int data[] , int first, int last){
+        if(first < last){
+            int mid = (first + last) /2;
+            mergeSort(data, first, mid);
+            mergeSort(data, mid + 1, last);
+            Merge(data, first, mid, last);
+        }
+    }
+        
     public static void main(String[] args) {
         // TODO code application logic here
     }
